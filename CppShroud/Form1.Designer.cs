77c83e623654dcf52f7620c9394dc99fdf1dfefc
BriefBassoon117 <115414619+Nickyangtpe@users.noreply.github.com>
2024-06-06 22:14:06 +0800

    Add files via upload

---
+++ CppShroud/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace CppShroud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1034, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開始混淆C++";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(481, 482);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "//Original C++";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(499, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(547, 482);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "//Obfuscated C++";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 564);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CppShroud/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CppShroud
+{
+    public partial class Form1 : Form
+    {
+        private static Random random = new Random();
+        private static StringBuilder defines = new StringBuilder();
+        private static Dictionary<string, string> replacementDict = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            defines.Clear(); // 每次點擊按鈕時清空定義
+            replacementDict.Clear(); // 清空替換字典
+            string input = textBox1.Text;
+            string result = ReplaceStrings(input);
+            textBox2.Text = defines.ToString() + result;
+        }
+
+        static string ReplaceStrings(string input)
+        {
+            // 使用正則表達式來處理每一行
+            string[] lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.None);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (!lines[i].TrimStart().StartsWith("#") && !lines[i].Contains("//"))
+                {
+                    lines[i] = Regex.Replace(lines[i], "\"(?:\\\\\"|[^\"])*\"|\\S+", new MatchEvaluator(match =>
+                    {
+                        string originalWord = match.Value;
+                        // 檢查是否在引號中
+                        if (originalWord.StartsWith("\"") && originalWord.EndsWith("\""))
+                        {
+                            return originalWord;
+                        }
+
+                        string replacement;
+                        if (replacementDict.TryGetValue(originalWord, out replacement))
+                        {
+                            // 如果已經有替換的字，則直接返回
+                            return " " + replacement;
+                        }
+                        else
+                        {
+                            // 否則，生成一個新的替換的字
+                            int length = random.Next(5, 16); // 長度在5到15之間
+                            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                            StringBuilder result = new StringBuilder(length);
+
+                            // 確保第一個字符不是數字
+                            result.Append(chars[random.Next(52)]); // 從字母開始
+                            for (int j = 1; j < length; j++)
+                            {
+                                result.Append(chars[random.Next(chars.Length)]);
+                            }
+
+                            // 添加 #define 替換的字 被替換的字
+                            defines.AppendLine($"#define {result} {originalWord} ");
+                            replacementDict[originalWord] = result.ToString(); // 將替換的字添加到字典中
+
+                            return " " + result;
+                        }
+                    }));
+                }
+            }
+            return string.Join(Environment.NewLine, lines);
+        }
+    }
+}
